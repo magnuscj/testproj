36fa8741b27adb0af6ce14d2b430a409c68356d6
magnus <magnus.c.johansson@gmail.com>
2017-11-14 17:47:47 +0100

    Added a add recipe function

--- repo/foodplanner/cookbook.cc
+++ repo/foodplanner/cookbook.cc
@@ -3,20 +3,37 @@
 
 using namespace std;
 
+
 class cookbook
 {
+  
 
 
 public:
+
   //Constructor
   cookbook()
   {
+    head = NULL;    //Set head to null
     cout<<"cookbook\n";
-    myRecipe = new recipe();
   }
 
-private:
-  recipe *myRecipe;
+  //Add a new recipe
+  void addRecipe()
+  {
+    recipeNode = new node(); //Create a new recipe
+    recipeNode->rec = new recipe();//Set the new recipe 
+    head = recipeNode;         //Move the head to the new recipe
+  }
   
+private:
+ 
+  struct node
+  {
+    recipe *rec;
+    node *next;
+  };
+  node *head;
+  node *recipeNode;
 };
 
